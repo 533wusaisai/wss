d952429ea298b4f1b314f8751fc612b3fd04b553
wusaisai <53923594+533wusaisai@users.noreply.github.com>
2021-12-02 13:38:06 +0800

    deploy

---
+++ javascripts.js
@@ -0,0 +1,10 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8feaa11c809139e526d04a3ac80a1e59";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+console.log('欢迎访问wss技术博客');
